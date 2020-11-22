459ed9998800c9a4c258667f06cadba2f366b836
Michael <roninnarasu@gmail.com>
2020-11-22 14:03:54 +0100

    Skeletoni raining down fire in this MF

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private static Enemy instance;
+    public static Enemy Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private EnemyFSM fsm;
+    public GameObject projectile;
+    internal bool finishedFiring = true;
+
+    internal Animator animator;
+
+    private void Awake()
+    {
+        instance = this;
+        fsm = new EnemyFSM();
+        fsm.Initialize(this);
+
+        fsm.AddState(EnemyStateType.Idle, new IdleState());
+        fsm.AddState(EnemyStateType.Sweep, new SweepState());
+        fsm.AddState(EnemyStateType.Slam, new SlamState());
+        fsm.AddState(EnemyStateType.Rain, new RainState());
+
+        fsm.GotoState(EnemyStateType.Idle);
+    }
+
+    private void Update()
+    {
+        fsm.UpdateState();
+    }
+
+    public EnemyStateType GetRandomState()
+    {
+        EnemyStateType e = (EnemyStateType)Random.Range(1, 4);
+        Debug.Log(e);
+        return e;
+    }
+
+    public IEnumerator SpawnProjectiles(int num, float frequency)
+    {
+        for (int i=0; i<num; i++)
+        {
+            
+            GameObject p = Instantiate(projectile);
+            p.transform.position = new Vector3(Random.Range(-15.0f, 15.0f), 15.0f, -10f);
+            yield return new WaitForSeconds(frequency);
+        }
+        finishedFiring = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            Player.Instance.TakeDamage();
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyState.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyStateType { Idle, Sweep, Slam, Rain }
+
+public abstract class EnemyState
+{
+    protected EnemyFSM owner;
+    protected Enemy enemy;
+
+    public void Initialize(EnemyFSM _owner)
+    {
+        owner = _owner;
+        enemy = _owner.owner;
+    }
+
+    public abstract void Enter();
+    public abstract void Update();
+    public abstract void Exit();
+}
+
+public class IdleState : EnemyState
+{
+    float timer;
+    public override void Enter()
+    {
+        timer = 2.0f;
+        
+    }
+
+    public override void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0) owner.GotoState(EnemyStateType.Rain);
+    }
+
+    public override void Exit()
+    {
+
+    }
+}
+
+public class SweepState : EnemyState
+{
+    public override void Enter()
+    {
+        owner.GotoState(EnemyStateType.Idle);
+    }
+
+    public override void Update()
+    {
+
+    }
+
+    public override void Exit()
+    {
+
+    }
+}
+
+public class SlamState : EnemyState
+{
+    public override void Enter()
+    {
+        
+    }
+
+    public override void Update()
+    {
+        
+    }
+
+    public override void Exit()
+    {
+
+    }
+}
+
+public class RainState : EnemyState
+{
+    int numProjectiles = 5;
+    float projectileFrequency = 0.4f;
+
+    public override void Enter()
+    {
+        enemy.finishedFiring = false;
+        IEnumerator c = enemy.SpawnProjectiles(numProjectiles, projectileFrequency);
+        enemy.StartCoroutine(c);
+    }
+
+    public override void Update()
+    {
+        if (enemy.finishedFiring) owner.GotoState(EnemyStateType.Idle);
+    }
+
+    public override void Exit()
+    {
+
+    }
+}
